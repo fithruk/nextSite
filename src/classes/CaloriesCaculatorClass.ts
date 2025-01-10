180573b31f22f6935b9375479443932463a82990
Sergey Belov <sergeytrenerbelov@gmail.com>
2025-01-10 14:42:28 +0200

    Started develope calculate food class

--- src/classes/CaloriesCaculatorClass.ts
+++ src/classes/CaloriesCaculatorClass.ts
@@ -1,7 +1,11 @@
 import {
   ActivityOptionsTypes,
+  CaloriesCalculatorUserAsnswerType,
   FoodPriorityOptionsTypes,
 } from "@/components/Dashboard/CaloriesCalculator/CaloriesCalculator";
+
+import foodData from "../components/Dashboard/FoodGenerator/foodData.json";
+import { UserAnswerType } from "@/hooks/useUserSurvey";
 
 const activityMultipliers: Record<ActivityOptionsTypes, number> = {
   Basic: 1.2,
@@ -20,6 +24,19 @@ interface UserData {
   age: number; // в годах
   activity: ActivityOptionsTypes;
   foodPriority: FoodPriorityOptionsTypes;
+}
+
+type NutrientsProportionsType = {
+  carbohydrates: number;
+  proteins: number;
+  vegetables?: number;
+  fats: number;
+};
+
+enum FoodPriorityEnum {
+  growWeight = "Grow weight",
+  lossWeight = "Loss weight",
+  maintaneWeight = "Maintain weight",
 }
 
 class CaloriesCalculatorClass {
@@ -43,6 +60,78 @@ class CaloriesCalculatorClass {
     }
 
     return Math.round(totalCalories);
+  };
+
+  public static PrepareFoodPlan = (
+    answersFromCalculator: CaloriesCalculatorUserAsnswerType,
+    userFoodAnswers: UserAnswerType
+  ) => {
+    const totalCalories = this.CountMinimalCalories(answersFromCalculator);
+    const { foodPriority } = answersFromCalculator;
+    const nutritionProportions = this.GetNutritionProportions(foodPriority);
+    const carbsProducts = this.getCarbsProducts(
+      totalCalories,
+      nutritionProportions!,
+      userFoodAnswers
+    );
+  };
+
+  private static GetNutritionProportions = (
+    foodPriority: FoodPriorityOptionsTypes
+  ) => {
+    const growProportions: NutrientsProportionsType = {
+      proteins: 25,
+      fats: 25,
+      carbohydrates: 50,
+    };
+    const lossProportions: NutrientsProportionsType = {
+      proteins: 40,
+      fats: 25,
+      carbohydrates: 35,
+    };
+
+    switch (foodPriority) {
+      case FoodPriorityEnum.growWeight:
+        return growProportions;
+      case FoodPriorityEnum.lossWeight:
+        return lossProportions;
+      case FoodPriorityEnum.maintaneWeight:
+        return growProportions;
+
+      default:
+        break;
+    }
+  };
+
+  private static getCarbsProducts = (
+    totalCalories: number,
+    proportions: NutrientsProportionsType,
+    userFoodAnswers: UserAnswerType
+  ) => {
+    const carbs = foodData.carbohydrates;
+    const carbsShare = proportions.carbohydrates;
+
+    const caloriesShare = Math.floor((totalCalories * carbsShare) / 100);
+
+    const preferredProductsData = carbs.filter((f) =>
+      userFoodAnswers.carbohydrates.includes(f.name)
+    );
+
+    const everyProductCalShare = Math.floor(
+      caloriesShare / preferredProductsData.length
+    );
+
+    const productQuantities = preferredProductsData.map((product) => {
+      const carbsPerGram = product.carbs / product.calories; // Углеводы на 1 ккал
+      const grams = Math.floor(everyProductCalShare / (carbsPerGram * 4)); // Граммы продукта
+      return {
+        name: product.name,
+        grams,
+        calories: everyProductCalShare,
+      };
+    });
+
+    console.log(productQuantities);
   };
 }
 
